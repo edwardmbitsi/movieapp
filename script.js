13b045288606ddd62ca57545df3aacaddd280fec
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2023-02-09 23:18:15 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+//Initial References
+let movieNameRef = document.getElementById("movie-name");
+let searchBtn = document.getElementById("search-btn");
+let result = document.getElementById("result");
