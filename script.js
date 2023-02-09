48ec011727e9de2d2aed9875cacd2f2de3bfbdfd
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2023-02-09 23:26:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let result = document.getElementById("result");
 //Function to fetch data from API
 let getMovie = () => {
   let movieName = movieNameRef.value;
-  let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+  let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${8b423bd7}`;
   //If input field is empty
   if (movieName.length <= 0) {
     result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
